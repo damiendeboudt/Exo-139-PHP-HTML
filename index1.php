f3445c75b7ec7a4a50e43c389434cdc46e83f662
Damien <damien.deboudt@sfr.fr>
2023-01-12 18:27:21 +0100

    ok.

--- index1.php
+++ index1.php
@@ -1,21 +1,23 @@
-
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
+<head>
     <meta charset="utf-8">
     <title>PHP & HTML</title>
-  </head>
-  <body>
-    <h1>Liste des élèves</h1>
-    <!-- Instructions : Afficher la liste des élèves qui sont présents dans le tableau $students, enrobez les dans un div, un span ou un p -->
+</head>
+<body>
+<h1>Liste des élèves</h1>
+<!-- Instructions : Afficher la liste des élèves qui sont présents dans le tableau $students, enrobez les dans un div, un span ou un p -->
+<?php
+//students
+$students = ['Hulk', 'Iron Man', 'Wonder Woman', 'Black Widow', 'Malicia'];
+?>
+<ul>
     <?php
-        //students
-        $students = ['Hulk', 'Iron Man', 'Wonder Woman', 'Black Widow', 'Malicia'];
-     ?>
-     <ul>
-       <?php
-            // TODO votre code ici
-       ?>
-     </ul>
+    foreach ($students as $items) {
+        echo "<p>$items</p>";
+    }
+    ?>
+</ul>
 
-    <!-- Incluez le fichier index2.php ici bas de manière a obtenir la suite du code HTML. -->
+<!-- Incluez le fichier index2.php ici bas de manière a obtenir la suite du code HTML. -->
+<?php include "./index2.php";
--- index2.php
+++ index2.php
@@ -7,6 +7,9 @@
        <select  id="day">
            <?php
            // TODO list of day
+           for($i = 1; $i <= 31; $i++) {
+               echo "<option>$i</option>";
+           }
            ?>
        </select>
 
@@ -14,6 +17,9 @@
        <select  id="month">
            <?php
            // TODO list of month
+               for($i = 1; $i <= 12; $i++) {
+                   echo "<option>$i</option>";
+               }
            ?>
        </select>
 
@@ -21,7 +27,9 @@
        <select  id="year">
            <?php
            // TODO list of year 2018, 2019, 2020 --> Bonus : faites le avec une vraie date en PHP, renseignez vous sur la doc
-
+           for($i = 2018; $i <= 2023; $i++) {
+               echo "<option>$i</option>";
+           }
            ?>
        </select>
      </form>
